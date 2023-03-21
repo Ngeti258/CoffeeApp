7141238f83f22f03b617563fc6789405669dd6a0
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2023-03-21 13:13:14 +0300

    added navigation to the layout

--- app/src/main/java/com/example/coffeeapp/Farmer.kt
+++ app/src/main/java/com/example/coffeeapp/Farmer.kt
@@ -1,47 +1,59 @@
 package com.example.coffeeapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class Farmer : AppCompatActivity() {
-    lateinit var btnNewProduct:Button
-    lateinit var btnViewOrders:Button
-    lateinit var btnViewProfile:Button
-    lateinit var btnLogout:Button
-    private lateinit var mAuth: FirebaseAuth
 
-
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_farmer)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        btnNewProduct = findViewById(R.id.btn_new_product)
-        btnViewOrders = findViewById(R.id.btn_view_orders)
-        btnViewProfile = findViewById(R.id.btn_view_profile)
-        mAuth = FirebaseAuth.getInstance()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        btnLogout = findViewById(R.id.btn_logout)
-
-        btnLogout.setOnClickListener(){
-            mAuth.signOut()
-            val intent = Intent(this@Farmer,Login::class.java)
-            finish()
-            startActivity(intent)
+        navView.setNavigationItemSelectedListener {
+            it.isChecked = true
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(FarmerProductsFragment(),it.title.toString())
+                R.id.nav_products -> replaceFragment(HistoryFragment(),it.title.toString())
+                R.id.nav_history -> replaceFragment(ProductsFragment(),it.title.toString())
+                R.id.nav_profile -> replaceFragment(ProfileFragment(),it.title.toString())
+            }
+            true
         }
-        btnViewProfile.setOnClickListener(){
-            val intent = Intent(this@Farmer,Profile::class.java)
-            startActivity(intent)
-        }
-        btnViewOrders.setOnClickListener(){
-            val intent = Intent(this@Farmer,History::class.java)
-            startActivity(intent)
-        }
-        btnNewProduct.setOnClickListener(){
-            val intent = Intent(this@Farmer,FarmerProducts::class.java)
-            startActivity(intent)
-        }
+        // Set the home fragment as the default fragment
+        val defaultFragment = ProductsFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.framelayout, defaultFragment).commit()
+        title = "Home"
     }
-}
+    private fun replaceFragment(fragment: Fragment, title : String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.framelayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return false
+    }
+    }
+
+
+
