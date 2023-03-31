a9d275bcdbfb551b2f8c7e5966b04c8a3c407e01
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2023-03-31 17:23:12 +0300

    made changes to this file to facilitate rendering of cart products based on the user id

--- app/src/main/java/com/example/coffeeapp/customer/CartFragment.kt
+++ app/src/main/java/com/example/coffeeapp/customer/CartFragment.kt
@@ -5,29 +5,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.coffeeapp.R
-
+import com.example.coffeeapp.farmer.CartAdapter
+import com.example.coffeeapp.farmer.Product
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class CartFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
+    private lateinit var database: FirebaseDatabase
+    private lateinit var productList: MutableList<Product>
+    private lateinit var adapter: CartAdapter
+    private lateinit var productsRecyclerView: RecyclerView
+    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_cart, container, false)
-        return view
+        val rootView = inflater.inflate(R.layout.fragment_cart, container, false)
+
+        // Initialize database
+        database = FirebaseDatabase.getInstance()
+        databaseRef = database.reference.child("carts")
+
+        // Initialize RecyclerView and ProductAdapter
+        productList = mutableListOf()
+        adapter = CartAdapter(requireContext(), productList)
+        productsRecyclerView = rootView.findViewById(R.id.products_recyclerview)
+        productsRecyclerView.layoutManager = LinearLayoutManager(activity)
+        productsRecyclerView.adapter = adapter
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        // Attach listener to databaseRef
+        databaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                productList.clear()
+                for (productSnapshot in snapshot.children) {
+                    val product = productSnapshot.getValue(Product::class.java)
+                    if (product != null) {
+                        if(userId == product.userId){
+                            product?.let { productList.add(it) }
+                        }
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle database error
+            }
+        })
+        return rootView
     }
 
+    fun show(transaction: FragmentManager, s: String) {
 
+    }
 
-
+    companion object {
+        fun newInstance(): CartFragment {
+            return CartFragment()
+        }
+    }
 }
