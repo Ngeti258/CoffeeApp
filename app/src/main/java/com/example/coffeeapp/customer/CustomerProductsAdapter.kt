e04084a800b66035edcd39afb3771af8cfdf818d
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2023-03-31 17:25:11 +0300

    changes to make it possible for adding to cart

--- app/src/main/java/com/example/coffeeapp/customer/CustomerProductsAdapter.kt
+++ app/src/main/java/com/example/coffeeapp/customer/CustomerProductsAdapter.kt
@@ -1,95 +1,30 @@
 package com.example.coffeeapp.customer
 
 import android.content.Context
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.coffeeapp.R
 import com.example.coffeeapp.farmer.Product
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
 import java.text.DecimalFormat
 
-class CustomerProductsAdapter(private val context: Context, var productList: List<Product>) :
-    RecyclerView.Adapter<CustomerProductsAdapter.ProductViewHolder>(){
-    private lateinit var database: FirebaseDatabase
-    private lateinit var coffeeTypeDropdown: AutoCompleteTextView
-    private lateinit var coffeeGradeEditText: EditText
-    private lateinit var quantityEditText: EditText
-    private lateinit var priceEditText: EditText
-    private lateinit var auth: FirebaseAuth
-
-    private var imageUri: Uri? = null
-    private lateinit var storage: FirebaseStorage
-
-    private lateinit var adapter: CustomerProductsAdapter
-    private lateinit var productsRecyclerView: RecyclerView
-    private lateinit var databaseRef: DatabaseReference
-
-
-    private var onItemClickListener: OnItemClickListener = object : OnItemClickListener {
-        override fun onItemClick(product: Product) {
-
-//            val fragment = ProductsFragment()
-//            val transaction = parentFragmentManager.beginTransaction()
-//            transaction.replace(R.id.framelayout,fragment).commit()
-        }
-    }
-
-    interface OnItemClickListener {
-        fun onItemClick(product: Product)
-    }
-
-    fun setOnItemClickListener(listener: OnItemClickListener) {
-        onItemClickListener = listener
-    }
+class CustomerProductsAdapter(
+    private val context: Context,
+    private var productList: List<Product>
+) : RecyclerView.Adapter<CustomerProductsAdapter.ProductViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.customer_products_layout, parent, false)
-        val addButton = view.findViewById<Button>(R.id.add_to_cart)
-        addButton.setOnClickListener(){
-            if (imageUri != null) {
-                val storageRef = storage.reference
-                val imageRef = storageRef.child("product_images")
-                val uploadTask = imageRef.putFile(imageUri!!)
-                uploadTask.continueWithTask { task ->
-                    if (!task.isSuccessful) {
-                        task.exception?.let {
-                            throw it
-                        }
-                    }
-                    imageRef.downloadUrl
-                }.addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val downloadUri = task.result
-                        // Add download URL to product object
-                        //product.imageUrl = downloadUri.toString()
-                        // Add product to Firebase Realtime Database
-                        val productsRef = database.getReference("orders")
-                        productsRef.push().setValue("orders")
-//                            .addOnSuccessListener {
-//                                Toast.makeText(
-//                                    activity?.applicationContext,
-//                                    "added to cart successfully",
-//                                    Toast.LENGTH_SHORT
-//                                ).show()
-//
-//
-//                            }
-
-                    }
-                }
-            }
-
-        }
-
-
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.customer_products_layout, parent, false)
         return ProductViewHolder(view)
     }
 
@@ -100,18 +35,9 @@ class CustomerProductsAdapter(private val context: Context, var productList: Lis
         holder.coffeeTypeTV.text = product.coffeeType
         holder.coffeeGradeTV.text = " ${product.coffeeGrade}"
 
-//        val quantityFormat = DecimalFormat("#,###.#")
-//        val formattedQuantity = quantityFormat.format(product.quantity)
-//        holder.quantityTextView.text = "${formattedQuantity}kg"
-
         val priceFormat = DecimalFormat("Ksh#,###.## per kilogram")
         val formattedPrice = priceFormat.format(product.price)
         holder.priceTV.text = formattedPrice
-
-        holder.productIV.setOnClickListener() {
-            onItemClickListener.onItemClick(product)
-
-        }
 
         // Load product image using Glide library
         if (product.imageUrl != null && product.imageUrl!!.isNotBlank()) {
@@ -119,26 +45,71 @@ class CustomerProductsAdapter(private val context: Context, var productList: Lis
         } else {
             holder.productIV.setImageResource(R.drawable.baseline_add_photo_alternate_24)
         }
-    }
 
+        holder.addButton.setOnClickListener {
+            val cartItem = CartItem(
+                product.coffeeType,
+                product.coffeeGrade,
+                product.price,
+                product.quantity,
+                product.imageUrl,
+                FirebaseAuth.getInstance().currentUser?.uid ?: ""
+            )
 
+            // add the cart item to the database
+            val cartRef = FirebaseDatabase.getInstance().getReference("carts")
+            cartItem.userId.let { it1 ->
+                cartItem.coffeeType.let { it2 ->
 
+                        if (it1 != null) {
+                            if (it2 != null) {
+                                cartRef.push().setValue(cartItem)
+                                    .addOnSuccessListener {
+                                        Toast.makeText(
+                                            context,
+                                            "Added to cart successfully",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                        val cartFragment = CartFragment.newInstance()
+                                        cartFragment.show(
+                                            (context as FragmentActivity).supportFragmentManager,
+                                            "CartFragment"
+                                        )
+                                    }.addOnFailureListener {
+                                        Toast.makeText(
+                                            context,
+                                            "Failed to add to cart: ${it.message}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                            }
+                        }
+                    }
 
-    override fun getItemCount() = productList.size
-
-    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        val coffeeTypeTV: TextView = itemView.findViewById(R.id.coffeeTypeTV)
-        val coffeeGradeTV: TextView = itemView.findViewById(R.id.coffeeGradeTV)
-        val priceTV: TextView = itemView.findViewById(R.id.coffeePriceTV)
-        val productIV: ImageView = itemView.findViewById(R.id.imageView)
-        val addButton : Button = itemView.findViewById(R.id.add_to_cart)
-        override fun onClick(v: View?) {
-            TODO("Not yet implemented")
+            }
         }
 
 
     }
 
+    override fun getItemCount() = productList.size
 
+    inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val coffeeTypeTV: TextView = itemView.findViewById(R.id.coffeeTypeTV)
+        val coffeeGradeTV: TextView = itemView.findViewById(R.id.coffeeGradeTV)
+        val priceTV: TextView = itemView.findViewById(R.id.coffeePriceTV)
+        val productIV: ImageView = itemView.findViewById(R.id.imageView)
+        val addButton: Button = itemView.findViewById(R.id.add_to_cart)
+    }
+}
+
+class CartItem(coffeeType: String?, coffeeGrade: String?, price: Double?, quantity: Double?, imageUrl: String?, s: String) {
+
+
+    val userId: String? = FirebaseAuth.getInstance().currentUser?.uid
+    val coffeeGrade: String? = coffeeGrade
+    val price : Double? = price
+    val imageUrl : String? = imageUrl
+    val coffeeType: String? = coffeeType
 
 }
